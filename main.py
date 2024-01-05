ae9bbefcf401cacd370d51d3613af3d62526609b
SergeyCherevichenko <cherevichenkosn@gmail.com>
2024-01-05 18:21:23 +0500

    change main

--- main.py
+++ main.py
@@ -2,38 +2,6 @@ from home_work_notebook.home_work_model.home_work_filehandler.FileHandlerJson im
 from home_work_notebook.home_work_model.home_work_service_note.ServiceNote import ServiceNote
 from home_work_notebook.home_work_view.ConsoleUI import ConsoleUI
 
-# service1 = ServiceNote(FileHandlerJson())
-# service1.add_note("Доброе утро","Как дела?")
-# service1.add_note("Добрый день","Как дела днем?")
-# service1.add_note("Добрый вечер","Как дела вечером?")
-# service1.to_string()
-#
-#
-# print("++++++++++++++++++++++++")
-#
-# service1.save()
-# service1.remove_notes()
-# service1.to_string()
-# print("++++++++++++++++++++++")
-# #service1.read()
-# service1.to_string()
-# print("+++++++++++++++++++++++++")
-#
-# print("+++++++++++++++++++++++++++")
-# print(service1.to_string())
-# #service1.read()
-# print(service1.to_string())
-# service1.read()
-# print("******************************")
-# service1.to_string()
-# service1.add_note("qgdskjahk","kahdas")
-# service1.add_note("yu","115")
-# print("===============================")
-# #service1.to_string()
-# service1.note_sort_by_data_creating()
-# service1.to_string()
-#
-# #
 view = ConsoleUI()
 view.start()
 
